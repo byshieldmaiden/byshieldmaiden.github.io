24ebdca90edccf65ab5c6621327b480bb6632916
Lisa Rabey <lrabey@gmail.com>
2017-03-28 17:13:37 -0400

    added https to api call in js/pugs2.js

---
+++ js/pugs3.js
@@ -0,0 +1,28 @@
+
+//Tells the script we're about to do something
+$(document).ready(function() {
+
+ function showPugPhoto () {
+  var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+  var pug = $(this).text();
+  var flickrOptions = {
+    tags: "pug, pugs",
+    format: "json"
+  } // end FlickrOptions
+  //Time to make it pretty
+   var displayPhotos = function(data){
+      var photoHTML = '<ul>';
+     $.each(data.items,function(i,photo) {
+      photoHTML += '<li>';
+      photoHTML += '<a href="' + photo.link + '" class="image">';
+      photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+        }); // end each
+      photoHTML += '</ul>';
+    $('#photos').html(photoHTML);
+  }//end making it pretty
+      $.getJSON(flickerAPI, flickrOptions, displayPhotos);
+    }; //end showPugPhoto function
+  showPugPhoto ();
+  $('button').click(showPugPhoto); //adding the button
+
+}); //end of the world
